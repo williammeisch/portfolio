98170458b9318e0303b1602378cd8d7fd2481d69
williammeisch <willmeisch@gmail.com>
2026-03-03 23:19:03 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function scrollToAbout() {
-  document.getElementById("about").scrollIntoView({
-    behavior: "smooth"
-  });
-}
+particlesJS("particles-js", {
+  "particles": {
+    "number": { "value": 50, "density": { "enable": true, "value_area": 800 } },
+    "color": { "value": ["#ffffff", "#ff0000", "#444444"] },
+    "shape": { "type": "circle" },
+    "opacity": { "value": 0.4, "random": true, "anim": { "enable": true, "speed": 1, "opacity_min": 0.1, "sync": false } },
+    "size": { "value": 2, "random": true },
+    "line_linked": { "enable": false },
+    "move": {
+      "enable": true,
+      "speed": 1.2,
+      "direction": "top",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+    }
+  },
+  "interactivity": { "events": { "onhover": { "enable": false }, "onclick": { "enable": false } } },
+  "retina_detect": true
+});
